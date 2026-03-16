bb8ae2d6c1fbe5f4d5d265a15722eb5d02070320
pariwats <dragonchaiwut@gmail.com>
2026-03-16 18:17:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
-const btn = document.getElementById("openBtn")
+/* OPEN INVITATION */
 
-btn.onclick=function(){
+const openBtn=document.getElementById("openBtn")
+const music=document.getElementById("bgMusic")
+
+openBtn.onclick=function(){
 
 document.getElementById("intro").style.display="none"
 document.getElementById("website").style.display="block"
 
-document.getElementById("music").play()
+music.play()
 
 }
 
 
+/* MUSIC BUTTON */
+
+const musicBtn=document.getElementById("musicToggle")
+
+musicBtn.onclick=function(){
+
+if(music.paused){
+
+music.play()
+musicBtn.innerText="🔊"
+
+}else{
+
+music.pause()
+musicBtn.innerText="🔇"
+
+}
+
+}
+
 
 /* COUNTDOWN */
 
-var weddingDate = new Date("Dec 8, 2026 00:00:00").getTime()
+var weddingDate=new Date("Dec 8, 2026 00:00:00").getTime()
 
 setInterval(function(){
 
-var now = new Date().getTime()
+var now=new Date().getTime()
+var distance=weddingDate-now
 
-var distance = weddingDate - now
-
-var days = Math.floor(distance/(1000*60*60*24))
-var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60))
-var minutes = Math.floor((distance%(1000*60*60))/(1000*60))
-var seconds = Math.floor((distance%(1000*60))/1000)
+var days=Math.floor(distance/(1000*60*60*24))
+var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60))
+var minutes=Math.floor((distance%(1000*60*60))/(1000*60))
+var seconds=Math.floor((distance%(1000*60))/1000)
 
 updateFlip("days",days)
 updateFlip("hours",hours)
@@ -32,7 +54,6 @@ updateFlip("minutes",minutes)
 updateFlip("seconds",seconds)
 
 },1000)
-
 
 
 function updateFlip(id,value){
@@ -44,10 +65,8 @@ if(el.innerText!=value){
 el.classList.add("flip")
 
 setTimeout(()=>{
-
 el.innerText=value
 el.classList.remove("flip")
-
 },250)
 
 }
@@ -55,12 +74,9 @@ el.classList.remove("flip")
 }
 
 
-
 /* COPY COLOR */
 
-const colors=document.querySelectorAll(".color")
-
-colors.forEach(c=>{
+document.querySelectorAll(".color").forEach(c=>{
 
 c.onclick=function(){
 
@@ -74,43 +90,56 @@ document.getElementById("copyMsg").innerText=hex+" copied!"
 
 })
 
-/* PETAL GENERATOR */
 
-const container = document.querySelector(".petals")
+/* PETAL SYSTEM */
 
-for(let i=0;i<20;i++){
+const back=document.querySelector(".back")
+const mid=document.querySelector(".mid")
+const front=document.querySelector(".front")
 
-const petal=document.createElement("div")
 
-petal.classList.add("petal")
+function spawnPetal(layer,minSize,maxSize,minSpeed,maxSpeed){
 
-/* random color */
+const petal=document.createElement("img")
 
-if(Math.random()>0.5){
+petal.src="petal.png"
+petal.className="petal"
 
-petal.classList.add("pink")
+petal.style.left=Math.random()*100+"vw"
 
-}else{
-
-petal.classList.add("blue")
-
-}
-
-/* random size */
-
-let size=20+Math.random()*25
-
+const size=Math.random()*(maxSize-minSize)+minSize
 petal.style.width=size+"px"
-petal.style.height=size+"px"
 
-/* random position */
+const duration=Math.random()*(maxSpeed-minSpeed)+minSpeed
+petal.style.animationDuration=duration+"s"
 
-petal.style.left=Math.random()*100+"%"
+const rotate=Math.random()*360
+petal.style.transform="rotate("+rotate+"deg)"
 
-/* random speed */
+petal.style.animationDelay=Math.random()*2+"s"
 
-petal.style.animationDuration=8+Math.random()*10+"s"
+layer.appendChild(petal)
 
-container.appendChild(petal)
+setTimeout(()=>{
+petal.remove()
+},duration*1000)
 
 }
+
+
+/* INITIAL PETALS */
+
+for(let i=0;i<4;i++){
+
+spawnPetal(back,20,30,14,18)
+spawnPetal(mid,30,40,12,16)
+spawnPetal(front,40,55,10,14)
+
+}
+
+
+/* CONTINUOUS PETALS */
+
+setInterval(()=>spawnPetal(back,20,30,14,18),2600)
+setInterval(()=>spawnPetal(mid,30,40,12,16),2000)
+setInterval(()=>spawnPetal(front,40,55,10,14),1600)
